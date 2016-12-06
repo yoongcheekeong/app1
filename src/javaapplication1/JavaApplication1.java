4deb30e742f9765c729b925600ce4bf4088ba60a
User <User@User-PC>
2016-12-06 17:38:22 +0800

    App1

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=3, b=2, c=1;
+            if (a>c){
+                a = b = c = 1;
+
+                System.out.println(b);
+
+    }
+    
+}
